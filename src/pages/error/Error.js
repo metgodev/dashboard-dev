2e143ee897a2e7d3f026a9eb950a15d5b82832b9
DorNakash <dorx33@gmail.com>
2021-12-08 14:46:52 +0200

    Changed the background of the error page + centered it + button

--- src/pages/error/Error.js
+++ src/pages/error/Error.js
@@ -1,41 +1,31 @@
-import React from "react";
+import React from 'react';
 import { Grid, Paper, Typography, Button } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import classnames from "classnames";
-
+import Lottie from 'react-lottie';
+import animatedErr from '../../Assets/lottieFiles/AnimatedError404.json'
 // styles
 import useStyles from "./styles";
-
-// logo
-import logo from "./logo.svg";
 
 export default function Error() {
   let classes = useStyles();
 
+  const defaultOptions = {
+    loop: true,
+    autoplay: true, 
+    animationData: animatedErr,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
   return (
     <Grid container className={classes.container}>
-      <Paper classes={{ root: classes.paperRoot }}>
-        <Typography
-          variant="h1"
-          color="primary"
-          className={classnames(classes.textRow, classes.errorCode)}
-        >
-          404
-        </Typography>
-        <Typography variant="h5" color="primary" className={classes.textRow}>
-          Oops. Looks like the page you're looking for no longer exists
-        </Typography>
-        <Typography
-          variant="h6"
-          color="text"
-          colorBrightness="secondary"
-          className={classnames(classes.textRow, classes.safetyText)}
-        >
-          But we're here to bring you back to safety
-        </Typography>
+      <div className={classes.divRoot}>
+      <Lottie width={500} height={500} options={defaultOptions} autoplay={true} loop={true}/>        
         <Button
           variant="contained"
-          color="primary"
+          color="secondary"
           component={Link}
           to="/"
           size="large"
@@ -43,7 +33,7 @@ export default function Error() {
         >
           Back to Home
         </Button>
-      </Paper>
+      </div>
     </Grid>
   );
 }
