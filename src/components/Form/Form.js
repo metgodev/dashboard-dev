3067c53695272160e1d1e8bd4b3b397177f023ad
Alex Levenberg <zeronid@gmail.com>
2022-07-31 13:49:43 +0300

    FIX: bug in form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -30,10 +30,14 @@ const MyForm = React.memo(({ fields, data, options, submitFunction, validiationF
   const [itemsToSend, setItemsToSend] = useState([])
 
   useEffect(() => {
-    setResizableText(data['description'])
+    setResizableText(data.description)
     setTimes(data.openingHours)
     setChosenImage(data.coverImageFileId)
   }, [data])
+
+  const getDataForForm = () => {
+    return { ...data }
+  }
 
   return (
     <>
@@ -42,132 +46,54 @@ const MyForm = React.memo(({ fields, data, options, submitFunction, validiationF
           onSubmit={(values) => {
             submitFunction({ ...values, description: resizableText, openingHours: times, objectIds: itemsToSend, coverImageFileId: chosenImage })
           }}
-          initialValues={data}
+          initialValues={getDataForForm}
           validate={validiationFunction}
-          render={({ handleSubmit, values }) => (
-            <form onSubmit={handleSubmit} noValidate className={classes.form}>
-              <Grid container spacing={2} className={classes.gridContainer}>
-                {fields.map(({ type, field, title, size }) => (
-                  <Grid key={field} item xs={size === 'small' ? 3 : size === 'medium' ? 6 : 12} className={type === 'googleAutocomplete' ? '' : classes.item}>
-                    {type === "textfield" && (
-                      <TextField
-                        label={title}
-                        name={field}
-                      />
-                    )}
-                    {type === "number" && (
-                      <TextField
-                        label={title}
-                        name={field}
-                        type={'number'}
-                      />
-                    )}
-                    {type === "picker" && (
-                      <Select label={title} name={field} required={true}>
-                        {options && options[field].map((item) => (
-                          <MenuItem key={item.value} value={item.value}>{item.name}</MenuItem>
-                        ))}
-                      </Select>
-                    )}
-                    {type === 'datePicker' &&
-                      <LocalizationProvider dateAdapter={AdapterDateFns}>
-                        <DatePicker
+          render={({ handleSubmit, values }) => {
+            return (
+              <form onSubmit={handleSubmit} noValidate className={classes.form}>
+                <Grid container spacing={2} className={classes.gridContainer}>
+                  {fields.map(({ type, field, title, size }) => (
+                    <Grid key={field} item xs={size === 'small' ? 3 : size === 'medium' ? 6 : 12} className={type === 'googleAutocomplete' ? '' : classes.item}>
+                      {type === "textfield" && (
+                        <TextField
                           label={title}
                           name={field}
-                          inputFormat={'dd/MM/yyyy'}
                         />
-                      </LocalizationProvider >
-                    }
-                    {type === 'timePicker' &&
-                      <LocalizationProvider dateAdapter={AdapterDateFns}>
-                        <MuiRffTimePicker
+                      )}
+                      {type === "number" && (
+                        <TextField
                           label={title}
                           name={field}
-                          closeOnSelect={true}
-                          ampm={false}
+                          type={'number'}
                         />
-                      </LocalizationProvider>
-                    }
-                    {type === "tagsPicker" && options[field].length > 0 && values[field] && (
-                      <Autocomplete
-                        label={title}
-                        name={field}
-                        multiple={true}
-                        variant="outlined"
-                        options={
-                          options[field].map((item) => ({
-                            label: item.title,
-                            value: item.id,
-                          }))
-                        }
-                        getOptionValue={(option) => option.value}
-                        getOptionLabel={(option) => option.label}
-                        disableCloseOnSelect={true}
-                        renderOption={(props, option, { selected }) => (
-                          <li {...props}>
-                            <MuiCheckbox
-                              style={{ marginRight: 8 }}
-                              checked={selected}
-                            />
-                            {option.label}
-                          </li>
-                        )}
-                      />
-                    )}
-                    {type === "checkbox" && (
-                      <Switches
-                        key={field}
-                        name={field}
-                        required={true}
-                        data={{ label: title, value: true }}
-                      />
-                    )}
-                    {type === "googleAutocomplete" && (
-                      <GoogleAutocomplete
-                        setFatherValue={setExternalValues}
-                        text={term('or_search_on_map')}
-                      />
-                    )}
-                    {type === "MapPicker" && (
-                      <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-                        <MapPick
-                          setFatherValue={setExternalValues}
-                          point={data["point"]}
-                          containerStyle={{ width: '90vw', height: '50vh' }}
-                          isLoaded={isLoaded}
-                        />
-                      </div>
-                    )}
-                    {type === 'timesPicker' && (
-                      <TimePicker
-                        title={title}
-                        realData={times}
-                        setTimes={(newTimes) => {
-                          setTimes(newTimes)
-                        }}
-                      />
-                    )}
-                    {type === 'textAreaSizeable' && (
-                      <TextareaAutosize
-                        aria-label={field}
-                        style={{ height: '200px', fontSize: '16px', paddingTop: '10px', overflowY: 'hidden' }}
-                        className={classes.resizeTextField}
-                        placeholder={term('description')}
-                        value={resizableText}
-                        minRows={3}
-                        onChange={(e) => { setResizableText(e.target.value) }}
-                      />
-                    )}
-                    {type === 'imagePicker' && (
-                      <ImagePicker
-                        title={IMAGE_PICKER_TITLE}
-                        pictures={values[field]}
-                        setChosenImage={setChosenImage}
-                        chosenImage={chosenImage}
-                      />
-                    )}
-                    {type === 'draggableListWithPickerAndImages' && options[field].length > 0 && values[field] && (
-                      <>
+                      )}
+                      {type === "picker" && (
+                        <Select label={title} name={field} required={true}>
+                          {options && options[field].map((item) => (
+                            <MenuItem key={item.value} value={item.value}>{item.name}</MenuItem>
+                          ))}
+                        </Select>
+                      )}
+                      {type === 'datePicker' &&
+                        <LocalizationProvider dateAdapter={AdapterDateFns}>
+                          <DatePicker
+                            label={title}
+                            name={field}
+                            inputFormat={'dd/MM/yyyy'}
+                          />
+                        </LocalizationProvider >
+                      }
+                      {type === 'timePicker' &&
+                        <LocalizationProvider dateAdapter={AdapterDateFns}>
+                          <MuiRffTimePicker
+                            label={title}
+                            name={field}
+                            closeOnSelect={true}
+                            ampm={false}
+                          />
+                        </LocalizationProvider>
+                      }
+                      {type === "tagsPicker" && options[field].length > 0 && values[field] && (
                         <Autocomplete
                           label={title}
                           name={field}
@@ -192,51 +118,131 @@ const MyForm = React.memo(({ fields, data, options, submitFunction, validiationF
                             </li>
                           )}
                         />
-                        <DraggableList
-                          items={values[field]}
-                          names={options[field]}
-                          setItemsToSend={setItemsToSend}
-                          itemsToSend={itemsToSend}
+                      )}
+                      {type === "checkbox" && (
+                        <Switches
+                          key={field}
+                          name={field}
+                          required={true}
+                          data={{ label: title, value: true }}
                         />
+                      )}
+                      {type === "googleAutocomplete" && (
+                        <GoogleAutocomplete
+                          setFatherValue={setExternalValues}
+                          text={term('or_search_on_map')}
+                        />
+                      )}
+                      {type === "MapPicker" && (
+                        <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+                          <MapPick
+                            setFatherValue={setExternalValues}
+                            point={data["point"]}
+                            containerStyle={{ width: '90vw', height: '50vh' }}
+                            isLoaded={isLoaded}
+                          />
+                        </div>
+                      )}
+                      {type === 'timesPicker' && (
+                        <TimePicker
+                          title={title}
+                          realData={times}
+                          setTimes={(newTimes) => {
+                            setTimes(newTimes)
+                          }}
+                        />
+                      )}
+                      {type === 'textAreaSizeable' && (
+                        <TextareaAutosize
+                          aria-label={field}
+                          style={{ height: '200px', fontSize: '16px', paddingTop: '10px', overflowY: 'hidden' }}
+                          className={classes.resizeTextField}
+                          placeholder={term('description')}
+                          value={resizableText}
+                          minRows={3}
+                          onChange={(e) => { setResizableText(e.target.value) }}
+                        />
+                      )}
+                      {type === 'imagePicker' && (
                         <ImagePicker
                           title={IMAGE_PICKER_TITLE}
-                          data={
-                            {
-                              ids: values[field],
-                              pictures: options[field].map(item => ({
-                                id: item?.id,
-                                url: item?.gallery[0]?.file?.url,
-                                pictureId: item?.gallery[0]?.fileId
-                              }))
-                            }
-                          }
+                          pictures={values[field]}
                           setChosenImage={setChosenImage}
                           chosenImage={chosenImage}
                         />
-                      </>
-                    )
-                    }
-                  </Grid>
-                ))}
-              </Grid>
-              {isPartOfStepper &&
-                <Box className={
-                  orientation === 'rtl' ?
-                    classes.submitButtonLeft : classes.submitButtonRight
-                }>
-                  <Button variant="contained" type="submit">{term('next')}</Button>
-                </Box>
-              }
-              {!isPartOfStepper &&
-                <Box className={
-                  orientation === 'rtl' ?
-                    classes.submitButtonLeft : classes.submitButtonRight
-                }>
-                  <Button variant="contained" type="submit">{term('submit')}</Button>
-                </Box>
-              }
-            </form >
-          )
+                      )}
+                      {type === 'draggableListWithPickerAndImages' && options[field].length > 0 && values[field] && (
+                        <>
+                          <Autocomplete
+                            label={title}
+                            name={field}
+                            multiple={true}
+                            variant="outlined"
+                            options={
+                              options[field].map((item) => ({
+                                label: item.title,
+                                value: item.id,
+                              }))
+                            }
+                            getOptionValue={(option) => option.value}
+                            getOptionLabel={(option) => option.label}
+                            disableCloseOnSelect={true}
+                            renderOption={(props, option, { selected }) => (
+                              <li {...props}>
+                                <MuiCheckbox
+                                  style={{ marginRight: 8 }}
+                                  checked={selected}
+                                />
+                                {option.label}
+                              </li>
+                            )}
+                          />
+                          <DraggableList
+                            items={values[field]}
+                            names={options[field]}
+                            setItemsToSend={setItemsToSend}
+                            itemsToSend={itemsToSend}
+                          />
+                          <ImagePicker
+                            title={IMAGE_PICKER_TITLE}
+                            data={
+                              {
+                                ids: values[field],
+                                pictures: options[field].map(item => ({
+                                  id: item?.id,
+                                  url: item?.gallery[0]?.file?.url,
+                                  pictureId: item?.gallery[0]?.fileId
+                                }))
+                              }
+                            }
+                            setChosenImage={setChosenImage}
+                            chosenImage={chosenImage}
+                          />
+                        </>
+                      )
+                      }
+                    </Grid>
+                  ))}
+                </Grid>
+                {isPartOfStepper &&
+                  <Box className={
+                    orientation === 'rtl' ?
+                      classes.submitButtonLeft : classes.submitButtonRight
+                  }>
+                    <Button variant="contained" type="submit">{term('next')}</Button>
+                  </Box>
+                }
+                {!isPartOfStepper &&
+                  <Box className={
+                    orientation === 'rtl' ?
+                      classes.submitButtonLeft : classes.submitButtonRight
+                  }>
+                    <Button variant="contained" type="submit">{term('submit')}</Button>
+                  </Box>
+                }
+              </form >
+            )
+          }
           }
         />
       }
