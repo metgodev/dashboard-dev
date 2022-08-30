c69fd069cd419535132bd43039f69867eee030ea
Alex Levenberg <zeronid@gmail.com>
2022-08-30 13:49:28 +0300

    FIX: removed glow from header menus

--- src/themes/default.js
+++ src/themes/default.js
@@ -131,8 +131,6 @@ export default {
     },
     MuiMenu: {
       paper: {
-        boxShadow:
-          "0px 3px 11px 0px #E8EAFC, 0 3px 3px -2px #B2B2B21A, 0 1px 8px 0 #9A9A9A1A",
       },
     },
     MuiSelect: {
