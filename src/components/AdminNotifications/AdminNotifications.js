0ad0dfd0aad4076eb98ed9247ac3e03088e132ba
Alex Levenberg <zeronid@gmail.com>
2022-09-06 11:43:51 +0300

    FIX: notification modal fixes

--- src/components/AdminNotifications/AdminNotifications.js
+++ src/components/AdminNotifications/AdminNotifications.js
@@ -1,10 +1,11 @@
 import React, { useState, useCallback, useEffect } from 'react'
-import { Box, CircularProgress, Modal } from '@material-ui/core'
+import { Box, CircularProgress, IconButton, Modal } from '@material-ui/core'
 import term from '../../terms'
 import NotificationItem from './NotificationItem'
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from 'react-redux';
 import { set_admin_notification } from '../../REDUX/actions/main.actions'
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 
 function AdminNotifications({ open, businesses, events, points, tracks, products }) {
 
@@ -46,7 +47,10 @@ function AdminNotifications({ open, businesses, events, points, tracks, products
             aria-describedby="modal-modal-description"
         >
             <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-                <div style={{ borderRadius: '10px', flexDirection: 'column', paddingTop: '40px', display: 'flex', alignItems: 'center', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '70vw', height: '70%', backgroundColor: 'white' }}>
+                <div style={{ borderRadius: '10px', flexDirection: 'column', paddingTop: '40px', display: 'flex', alignItems: 'center', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '40vw', height: '70%', backgroundColor: 'white' }}>
+                    <IconButton onClick={handleClose} style={{ position: 'absolute', top: 10, left: 10 }}>
+                        < CloseOutlinedIcon />
+                    </IconButton>
                     <p style={{ fontWeight: 'bold', fontSize: '25px' }}>{term('notifications')}</p>
                     <Box style={{ width: '100%', height: '100%', padding: '5px', display: 'flex', justifyContent: "center", alignItems: 'center', }}>
                         {loading ?
@@ -55,27 +59,22 @@ function AdminNotifications({ open, businesses, events, points, tracks, products
                             <div>
                                 <NotificationItem
                                     text={`${term('you_have')} ${numberOfBusinesses} ${term('unapproved_businesses')}`}
-                                    buttonText={term('businesses')}
                                     onClick={() => buttonPressed('/businesses')}
                                 />
                                 <NotificationItem
                                     text={`${term('you_have')} ${numberOfEvents} ${term('unapproved_events')}`}
-                                    buttonText={term('events')}
                                     onClick={() => buttonPressed('/events')}
                                 />
                                 <NotificationItem
                                     text={`${term('you_have')} ${numberOfPoints} ${term('unapproved_points')}`}
-                                    buttonText={term('points')}
                                     onClick={() => buttonPressed('/locations')}
                                 />
                                 <NotificationItem
                                     text={`${term('you_have')} ${numberOfTracks} ${term('unapproved_tracks')}`}
-                                    buttonText={term('tracks')}
                                     onClick={() => buttonPressed('/routes')}
                                 />
                                 <NotificationItem
                                     text={`${term('you_have')} ${numberOfProducts} ${term('unapproved_products')}`}
-                                    buttonText={term('products')}
                                     onClick={() => buttonPressed('/products')}
                                 />
                             </div>
--- src/components/AdminNotifications/NotificationItem.js
+++ src/components/AdminNotifications/NotificationItem.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Button } from '@material-ui/core'
 
-function NotificationItem({ text, buttonText, onClick }) {
+function NotificationItem({ text, onClick }) {
     return (
         <p style={{ fontWeight: 'bold', display: 'flex', alignItems: 'center', gap: '20px', justifyContent: 'space-between' }}>
-            <span>{text}</span>
-            <Button style={{ width: '100px' }} onClick={onClick} variant='outlined'>{buttonText}</Button>
+            <Button style={{ width: '400px' }} onClick={onClick} variant='outlined'>{text}</Button>
         </p>
     )
 }
