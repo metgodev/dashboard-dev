95ec41e57474de4ecf35f307689fe83b7e719e3a
Alex Levenberg <zeronid@gmail.com>
2022-10-31 15:32:39 +0200

    FIX: bugs in loading businesses

--- src/Root/routes/MemberRoutes.js
+++ src/Root/routes/MemberRoutes.js
@@ -17,11 +17,11 @@ function MemberRoutes({ permissions, user }) {
         <Routes>
             <Route exact path={ROUTES.ROOT} element={<Protecte auth={permissions.main} loggedIn={user}><Navigate to={BUSINESS_OWNER_ROUTES.DASHBOARD} /></Protecte>} />
             <Route exact path={BUSINESS_OWNER_ROUTES.BUSINESSES} element={<Protecte auth={permissions.business} loggedIn={user}><Businesses /></Protecte>} />
-            <Route exact path={BUSINESS_OWNER_ROUTES.LOGIN} element={<BusinessRegister />} />
+            <Route exact path={BUSINESS_OWNER_ROUTES.LOGIN} element={<Login />} />
             <Route exact path={ROUTES.MAP} element={<Protecte auth={permissions.map} loggedIn={user}><Maps /></Protecte>} />
             <Route exact path={ROUTES.SUPPORT} element={<Protecte auth={permissions.support} loggedIn={user}><Support /></Protecte>} />
             <Route exact path={ROUTES.FAQ} element={<Protecte auth={permissions.faq} loggedIn={user}><FAQ /></Protecte>} />
-            <Route exact path={ROUTES.LOGIN} element={<Login />} />
+            <Route exact path={BUSINESS_OWNER_ROUTES.MAP} element={<Protecte auth={permissions.map} loggedIn={user}><Maps /></Protecte>} />
             <Route exact path={ROUTES.USERS} element={<Protecte auth={permissions.users} loggedIn={user}><UsersTable /></Protecte>} />
             <Route path={ROUTES.ERROR} element={<Protecte auth={permissions.error} loggedIn={user}><Error /></Protecte>} />
             <Route exact path={ROUTES.PROFILE} element={<Protecte auth={permissions.profile} loggedIn={user}><Profile /></Protecte>} />
--- src/components/MobileTable/MobileTable.js
+++ src/components/MobileTable/MobileTable.js
@@ -5,6 +5,8 @@ import useStyles from './styles'
 import { useSelector } from 'react-redux';
 import MobileTableEntity from './MobileTableEntity';
 import Toast from "../../utils/useToast";
+import BACK_ROUTES from '../../data/back_routes';
+import client from '../../API/metro'
 
 function MobileTable({ display, action }) {
 
@@ -12,19 +14,28 @@ function MobileTable({ display, action }) {
 
     const { area } = useSelector(s => s.mainRememberReducer)
     const tableChanged = useSelector(state => state.mainReducer.tableChanged)
+    const userDetails = useSelector(s => s.userReducer.userDetails)
 
-    const [data, setData] = useState([])
+    const [data, setData] = useState(null)
 
     useEffect(() => {
         (async () => {
-            try {
+            if (userDetails.roles.length === 1) {
                 setData([])
-                const res = await _get(display, { $limit: 1000, areaId: area.id, isAnonymous: false })
+                return
+            }
+            try {
+                const res =
+                    userDetails.roles[1].resourceIds.length === 0 ?
+                        await client.service(BACK_ROUTES[display.toUpperCase()]).find({ query: { $limit: 1000 } })
+                        :
+                        await client.service(BACK_ROUTES[display.toUpperCase()]).find({ query: { _id: { $limit: 1000, $in: userDetails.roles[1].resourceIds } } })
                 if (res) {
                     setData(res.data)
                 }
             }
             catch (e) {
+                console.log(e)
                 Toast()
             }
         })();
@@ -32,8 +43,8 @@ function MobileTable({ display, action }) {
 
     return (
         <Box className={classes.container}>
-            {data.length === 0 && <CircularProgress size={50} />}
-            {data.length > 0 && data.map(unit => {
+            {data === null && <CircularProgress size={50} />}
+            {data !== null && data.length > 0 && data.map(unit => {
                 return (
                     <MobileTableEntity entity={unit} onClick={action} display={display} />
                 )
